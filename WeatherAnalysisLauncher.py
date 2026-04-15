9501acaef775da5b443f5d72cdd41c1e6f081c69
PogiSyd <khan.bobby@gmail.com>
2026-04-15 19:26:17 +1000

    Add get_cards function for weather analysis

---
+++ WeatherAnalysisLauncher.py
@@ -0,0 +1,7 @@
+def get_cards(fit_file):
+    """Math engine for Temperature and Wind Impact"""
+    cards = [
+        {"type": "stat", "title": "Avg Temp", "value": "18°C", "trend": "Ideal"},
+        {"type": "stat", "title": "Wind Penalty", "value": "12W", "trend": "Headwind"}
+    ]
+    return cards
